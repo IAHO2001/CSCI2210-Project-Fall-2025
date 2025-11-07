76d5f351f2f9ee5c141d182bf85613e722ccd85d
JacobChavezENMU <jacob.n.chavez@enmu.edu>
2025-11-07 11:05:09 -0700

    MenuItem
    Added instance variables and methods for MenuItem class.

--- Stage 3/Project/MenuItem.java
+++ Stage 3/Project/MenuItem.java
@@ -1,3 +1,52 @@
+
 public class MenuItem {
+    //Instance variables
+    private int itemID;
+    private String name;
+    private double price;
+
+    /**
+     * Constructor
+     * @param name
+     * @param price
+     */
+    public MenuItem(String name, double price){
+        this.itemID = IDgenerator.generateItemID();
+        this.name = name;
+        this.price = price;
+    }
+
+    /**
+     * 
+     * @return int itemID
+     */
+    public int getItemID(){
+        return itemID;
+    }
+
+    /**
+     * 
+     * @return String name
+     */
+    public String getName(){
+        return name;
+    }
+
+    /**
+     * 
+     * @return double price
+     */
+    public double getPrice(){
+        return price;
+    }
+
+    /**
+     * 
+     * @param price
+     */
+    public void setPrice(double price){
+        this.price = price;
+    }
 
 }
+
